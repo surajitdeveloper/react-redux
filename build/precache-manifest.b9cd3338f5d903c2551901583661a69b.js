3d3eb0d969c5907e96021f3a6d26db916d626206
venkat <venkat@facileconsulting.com>
2021-02-20 08:37:35 +0530

    all

---
+++ build/precache-manifest.b9cd3338f5d903c2551901583661a69b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7390de76f562910974ce41a10bd73760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "497bb50c083764aa82d9",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "c7fcd83281efd396996b",
+    "url": "/static/js/2.f79e27e0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.f79e27e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "497bb50c083764aa82d9",
+    "url": "/static/js/main.52abf409.chunk.js"
+  },
+  {
+    "revision": "9010a7c7d05cbc65b3ab",
+    "url": "/static/js/runtime-main.4be616a3.js"
+  }
+]);
