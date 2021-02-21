7e6fb5cdef03fe8cb4bdd6f3b82b963866410e4e
venkat <venkat@facileconsulting.com>
2021-02-21 10:50:58 +0530

    change build file

---
+++ build/precache-manifest.898367985a26b6aa810fd85be3bbc990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b9e0c28eb1b80d3ffd61f64f064810",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bf93ccba79d5676c5ce",
+    "url": "/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "daafa9f4a001d64a7019",
+    "url": "/static/js/2.4ac1c62c.chunk.js"
+  },
+  {
+    "revision": "ca7cc1293ec719cab3b4b19149d4ba73",
+    "url": "/static/js/2.4ac1c62c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf93ccba79d5676c5ce",
+    "url": "/static/js/main.253d2798.chunk.js"
+  },
+  {
+    "revision": "9372afefddbc2867b6f1",
+    "url": "/static/js/runtime-main.02af016f.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.878c06064cd98b6f71d22a44ca7ab775.js"
+  "/precache-manifest.898367985a26b6aa810fd85be3bbc990.js"
 );
 
 self.addEventListener('message', (event) => {
